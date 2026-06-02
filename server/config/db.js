f564bfbe11963285e13c24043f482f23ece5d3dc
Vaishnavi Kharwade <vaishnavikharwade78@gmail.com>
2026-06-02 12:06:20 +0530

    backend connected mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,21 @@
+
+
 const mongoose = require("mongoose");
+const dns = require("dns");
 
 const connectDB = async () => {
+  const uri = process.env.MONGO_URI;
+  const dbName = process.env.DB_NAME || "ecom-site";
+
+  dns.setServers(["8.8.8.8", "1.1.1.1"]);
+  // console.log("Using DNS servers:", dns.getServers());
+
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(uri, { dbName });
     console.log("✅ MongoDB Connected");
   } catch (err) {
-    console.error("❌ MongoDB Connection Error:", err);
+    console.error("❌ Error:", err.message);
     process.exit(1);
   }
 };
--- server/server.js
+++ server/server.js
@@ -11,20 +11,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-connectDB();
+const startServer = async () => {
+  await connectDB();
 
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
+  // Routes
+  app.use("/api/auth", authRoutes);
+  app.use("/api/products", productRoutes);
 
-// Health check
-app.get("/", (req, res) => {
-  res.json({ message: "Backend running" });
-});
+  // Health check
+  app.get("/", (req, res) => {
+    res.json({ message: "Backend running" });
+  });
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+  // Start server
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
